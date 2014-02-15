e9770a3f2de6024c1364cf8b2ff4f8a8f86176ba
hyness <hyness@gmail.com>
2014-02-14 16:46:10 -0800

    Updated to Spring Boot 1.0.0.RC3 Removed unneeded @ResponseBody on controller methods

--- src/main/java/org/hyness/video/web/SearchController.java
+++ src/main/java/org/hyness/video/web/SearchController.java
@@ -10,7 +10,6 @@ import org.hyness.video.service.VideoService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -21,13 +20,11 @@ public class SearchController {
 	@Inject
 	private VideoService service;
 	
-	@ResponseBody
 	@RequestMapping(value = "/search/{term}", method = RequestMethod.GET)
 	public Result search(@PathVariable String term) {
 		return service.search(term);
 	}
 	
-	@ResponseBody
 	@RequestMapping(value = "/search/{term}/{hd}/{page}", method = RequestMethod.GET)
 	public Result searchPage(@PathVariable String term, @PathVariable boolean hd, @PathVariable int page) {
 		return service.search(term, hd, page);
