c2d6404b4fc3b9ce0cb51fd0078a1b679d1fa141
hyness <hyness@gmail.com>
2014-09-26 16:28:42 -0700

    Fixed encoding issue for static files Upgraded to tomcat 8 Added favicon

--- src/main/java/org/hyness/video/YouTubeApplication.java
+++ src/main/java/org/hyness/video/YouTubeApplication.java
@@ -2,13 +2,16 @@ package org.hyness.video;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.MimeMappings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
 @EnableAutoConfiguration
 @ComponentScan
-public class YouTubeApplication {
+public class YouTubeApplication implements EmbeddedServletContainerCustomizer {
 	public static void main(String[] args) {
 		SpringApplication.run(YouTubeApplication.class, args);
 	}
@@ -16,5 +19,12 @@ public class YouTubeApplication {
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
+	}
+
+	@Override
+	public void customize(ConfigurableEmbeddedServletContainer container) {
+		MimeMappings mappings = new MimeMappings(MimeMappings.DEFAULT);
+        mappings.add("html", "text/html;charset=utf-8");
+        container.setMimeMappings(mappings);
 	}
 }
