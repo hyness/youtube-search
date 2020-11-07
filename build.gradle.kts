7a680c2b5cf563df2eab2a7a250980902b5296d7
hyness <hyness@freshlegacycode.org>
2020-11-07 12:05:42 -0800

    --wip-- [skip ci]

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,35 +1,19 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-buildscript {
-    repositories {
-        gradlePluginPortal()
-    }
-    dependencies {
-        classpath("org.siouan:frontend-gradle-plugin-jdk11:4.0.1")
-    }
-}
-
-
-apply(plugin = "org.siouan.frontend-jdk11")
+import org.gradle.api.JavaVersion.*
 
 plugins {
     id("jacoco")
     id("org.sonarqube") version "2.8"
-    id("org.springframework.boot") version "2.2.4.RELEASE"
-    id("io.spring.dependency-management") version "1.0.9.RELEASE"
+    id("org.springframework.boot") version "2.3.5.RELEASE"
+    id("io.spring.dependency-management") version "1.0.10.RELEASE"
     id("com.github.kt3k.coveralls") version "2.9.0"
     id("org.siouan.frontend-jdk11") version "4.0.1"
-    kotlin("jvm") version "1.3.61"
-    kotlin("plugin.spring") version "1.3.61"
+    kotlin("jvm") version "1.4.10"
+    kotlin("plugin.spring") version "1.4.10"
 }
+
 group = "org.freshlegacycode"
-version = "1.0-SNAPSHSHOT"
-
-java.sourceCompatibility = JavaVersion.VERSION_11
-
-tasks.bootJar {
-    archiveVersion.set("")
-}
+java.sourceCompatibility = VERSION_11
+val frontEndDir =  "${projectDir}/src/main/frontend"
 
 repositories {
     mavenCentral()
@@ -41,56 +25,58 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.hibernate.validator:hibernate-validator")
     implementation("org.webjars:webjars-locator:0.33")
     implementation("org.webjars:jquery:1.12.1")
     implementation("org.webjars:bootstrap:3.3.7")
     compileOnly("org.springframework.boot:spring-boot-configuration-processor")
-//    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
     testImplementation("io.projectreactor:reactor-test")
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
+tasks{
+    compileKotlin {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = java.sourceCompatibility.majorVersion
+        }
+    }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "1.8"
+    processResources {
+        dependsOn("copyFrontendToBuild")
+    }
+
+    bootJar {
+        layered()
+    }
+
+    test {
+        useJUnitPlatform()
+        finalizedBy("jacocoTestReport")
+    }
+
+    jacocoTestReport {
+        reports {
+            xml.isEnabled = true
+            html.isEnabled = false
+        }
+    }
+
+    create<Copy>("copyFrontendToBuild") {
+        from("$frontEndDir/build")
+        into("${buildDir}/resources/main/static")
     }
 }
 
-val frontEndDir =  "${projectDir}/src/main/frontend"
 frontend {
-    nodeVersion.set("10.16.2")
+    nodeVersion.set("15.1.0")
     nodeInstallDirectory.set(file("$projectDir/.gradle/node"))
     packageJsonDirectory.set(file(frontEndDir))
     assembleScript.set("run build")
-    cleanScript.set("ci")
-    checkScript.set("run check")
-}
-
-tasks.test {
-    finalizedBy("jacocoTestReport")
-}
-
-tasks.withType<ProcessResources> {
-    dependsOn("copyFrontendToBuild")
-}
-
-tasks.create<Copy>("copyFrontendToBuild") {
-    from("$frontEndDir/build")
-    into("${buildDir}/resources/main/static")
-}
-
-tasks.jacocoTestReport {
-    reports {
-        xml.isEnabled = true
-        html.isEnabled = false
-    }
+    cleanScript.set("run clean")
 }
 
 coveralls {
